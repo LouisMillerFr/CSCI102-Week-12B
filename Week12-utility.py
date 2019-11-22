85e6b4fd94730ac2ce22a2ea051810392c1ac5b2
LouisMillerFr <louispaulmiller@gmail.com>
2019-11-21 23:21:58 -0700

    Added NotIn

--- Week12-utility.py
+++ Week12-utility.py
@@ -38,4 +38,10 @@ def Intersection(l1, l2):
             l.append(l1[i])
     return l
 
-
+def NotIn(l1, l2):
+    l = []
+    for i in range(len(l1)):
+        if l1[i] not in l2:
+            l.append(l1[i])
+    return l
+    
